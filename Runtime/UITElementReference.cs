70b7651390f911f0f150976bb8e4dfe9f54b1a5f
GePrasetyo <dhimasgppantoja@gmail.com>
2023-12-03 16:43:33 +0100

    Fixed Assembly

--- Runtime/UITElementReference.cs
+++ Runtime/UITElementReference.cs
@@ -1,18 +1,18 @@
-using System;
-using UnityEngine;
-using UnityEngine.UIElements;
+//using System;
+//using UnityEngine;
+//using UnityEngine.UIElements;
 
-namespace Majingari.UI {
-    /// <summary>
-    /// This is not working for now
-    /// </summary>
-    [Serializable]
-    public class UITElementReference {
-        [SerializeField] internal UIDocument uiDocument;
-    }
+//namespace Majingari.UI {
+//    /// <summary>
+//    /// This is not working for now
+//    /// </summary>
+//    [Serializable]
+//    public class UITElementReference {
+//        [SerializeField] internal UIDocument uiDocument;
+//    }
 
-    [Serializable]
-    public class SampleUIT : UITElementReference {
-        public Button testButton;
-    }
-}
+//    [Serializable]
+//    public class SampleUIT : UITElementReference {
+//        public Button testButton;
+//    }
+//}
